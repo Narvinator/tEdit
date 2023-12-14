6c8be358387925f72e8c9e41bdbe7d717edba82f
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-12-14 17:11:48 -0500

    fixed bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,9 +28,9 @@ export const getDb = async () => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 initdb();
