b1b33462745a2bfeff37240d8d5b2d398c424cd1
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-12-14 17:58:34 -0500

    more fixed bugs

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require("'workbox-recipes");
-const { CacheFirst,SaleWhileRevalidate } = require("workbox-strategies");
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate  } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
@@ -29,7 +29,7 @@ registerRoute(({ request }) => request.mode === "navigate", pageCache);
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new SaleWhileRevalidate({
+  new StaleWhileRevalidate({
     cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
